fe531fded9aa44d1d423157d1b86a9eade2b9471
Sayon Usman <sayonusman.ma@gmail.com>
2023-02-18 01:12:29 +0300

    Added createNewSection function in calculateAreaAndDisplay.js file.

--- js/calculateAreaAndDisplay.js
+++ js/calculateAreaAndDisplay.js
@@ -74,27 +74,72 @@ function calculateAreaAndDisplay(id) {
     }
 }
 
+function createNewSection(id, area, count) {
+    const calculationArea = document.getElementById('calculation-area');
+    const div = document.createElement('div');
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+    const sup1 = document.createElement('sup');
+    const sup2 = document.createElement('sup');
+    const button = document.createElement('button');
+
+    div.classList.add('d-flex');
+    div.classList.add('justify-content-between');
+    div.classList.add('mb-3');
+
+    p1.classList.add('ms-2');
+
+    button.classList.add('btn');
+    button.classList.add('btn-primary');
+    
+    p1.innerText = count.toString() + '. ' + id;
+    p2.innerText = area + 'cm';
+    sup1.innerText = '2';
+    sup2.innerText = '2';
+    button.innerText = 'Convert to m';
+
+    calculationArea.appendChild(div);
+    div.appendChild(p1);
+    div.appendChild(p2);
+    p2.appendChild(sup1);
+    button.appendChild(sup2);
+    div.appendChild(button);
+}
+
+let count = 1;
 
 document.getElementById('triangle-area-cal-btn').addEventListener('click', function() {
-    calculateAreaAndDisplay('triangle-area-cal-btn');
+    const area = calculateAreaAndDisplay('triangle-area-cal-btn');
+    createNewSection('Triangle', area, count);
+    count ++;
 });
 
 document.getElementById('rectangle-area-cal-btn').addEventListener('click', function() {
-    calculateAreaAndDisplay('rectangle-area-cal-btn');
+    const area = calculateAreaAndDisplay('rectangle-area-cal-btn');
+    createNewSection('Rectangle', area, count);
+    count ++;
 });
 
 document.getElementById('parallelogram-area-cal-btn').addEventListener('click', function() {
-    calculateAreaAndDisplay('parallelogram-area-cal-btn');
+    const area = calculateAreaAndDisplay('parallelogram-area-cal-btn');
+    createNewSection('Parall...', area, count);
+    count ++;
 });
 
 document.getElementById('rhombus-area-cal-btn').addEventListener('click', function() {
-    calculateAreaAndDisplay('rhombus-area-cal-btn');
+    const area = calculateAreaAndDisplay('rhombus-area-cal-btn');
+    createNewSection('Rhombus', area, count);
+    count ++;
 });
 
 document.getElementById('pentagon-area-cal-btn').addEventListener('click', function() {
-    calculateAreaAndDisplay('pentagon-area-cal-btn');
+    const area = calculateAreaAndDisplay('pentagon-area-cal-btn');
+    createNewSection('Pentagon', area, count);
+    count ++;
 });
 
 document.getElementById('ellipse-area-cal-btn').addEventListener('click', function() {
-    calculateAreaAndDisplay('ellipse-area-cal-btn');
+    const area = calculateAreaAndDisplay('ellipse-area-cal-btn');
+    createNewSection('Ellipse', area, count);
+    count ++;
 });
